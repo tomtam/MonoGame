69a2ae443dd031a4afa035a8737c58aca92eaf17
James Lupiani <lupiani@sickheadgames.com>
2013-07-22 15:08:55 -0500

    Added garbage-free enumerator to EffectParameterCollection.

--- MonoGame.Framework/Graphics/Effect/EffectParameterCollection.cs
+++ MonoGame.Framework/Graphics/Effect/EffectParameterCollection.cs
@@ -50,14 +50,56 @@ namespace Microsoft.Xna.Framework.Graphics
 			}
         }
 
-        public IEnumerator<EffectParameter> GetEnumerator()
+        public ParamEnumerator GetEnumerator()
         {
-            return ((IEnumerable<EffectParameter>)_parameters).GetEnumerator();
+            return new ParamEnumerator(this);
+        }
+
+        IEnumerator<EffectParameter> IEnumerable<EffectParameter>.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
-            return _parameters.GetEnumerator();
+            return GetEnumerator();
+        }
+
+        public struct ParamEnumerator : IEnumerator<EffectParameter>
+        {
+            private readonly EffectParameter[] _items;
+            private int _index;
+
+            public ParamEnumerator(EffectParameterCollection collection)
+            {
+                _items = collection._parameters;
+                _index = -1;
+            }
+
+            public EffectParameter Current
+            {
+                get { return _items[_index]; }
+            }
+
+            public void Dispose()
+            {
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                return _index < _items.Length;
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
         }
     }
 }
